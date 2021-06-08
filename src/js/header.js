73d14fcafe6ebb766599e8bde61dd1ac8f174848
Bogdan <bogdemona@mail.ru>
2021-06-08 23:34:19 +0300

    create: add

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+;
+(function () {
+	if (window.matchMedia('(max-width: 992px)').matches) {
+		return;
+	}
+
+	let headerPage = document.querySelector('.header-page');
+	window.addEventListener('scroll', function () {
+
+	});
+})();
