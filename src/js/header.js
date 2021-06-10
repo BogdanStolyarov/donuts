16f8924860cbf9e78de85df59531c156edbb029a
Bogdan <bogdemona@mail.ru>
2021-06-10 22:12:36 +0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -5,7 +5,12 @@
 	}
 
 	let headerPage = document.querySelector('.header-page');
-	window.addEventListener('scroll', function () {
 
+	window.addEventListener('scroll', function () {
+		if (window.pageYOffset > 0) {
+			headerPage.classList.add('is-active');
+		} else {
+			headerPage.classList.remove('is-active');
+		}
 	});
 })();
